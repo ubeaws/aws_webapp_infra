a80546a0fc000db63ef2c581555bcbb9a7ca0863
ubedimran <ubeaws@outlook.com>
2025-06-30 12:48:31 -0500

    Fix: formatted main.tf using terraform fmt

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,6 +1,6 @@
 module "vpc" {
-  source    = "../../modules/vpc"
-  vpc_cidr  = "10.0.0.0/16"
-  vpc_name  = "dev-vpc"
+  source   = "../../modules/vpc"
+  vpc_cidr = "10.0.0.0/16"
+  vpc_name = "dev-vpc"
 }
 
